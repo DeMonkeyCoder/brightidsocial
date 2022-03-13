f92d34ed393f0ddcde219485ea704c108624c53f
Ali Mahdiyar <alimahdiyar77@gmail.com>
2022-03-13 19:44:52 +0330

    add profile hashes to admin panel

--- core/admin.py
+++ core/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from core.models import SocialMedia, SocialMediaVariation
+from core.models import SocialMedia, SocialMediaVariation, ProfileHash
 
 admin.site.register(SocialMedia)
 admin.site.register(SocialMediaVariation)
+admin.site.register(ProfileHash)
