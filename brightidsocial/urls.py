61d2c92c2540681f1d9cbd793ccb96a8abedb640
Ali Mahdiyar <alimahdiyar77@gmail.com>
2022-04-24 09:37:33 +0430

    add baseurl in urls.py

--- brightidsocial/urls.py
+++ brightidsocial/urls.py
@@ -17,7 +17,9 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/v1/', include("core.api.v1.urls")),
-    path('api/docs/', include("core.api.docs.urls")),
+    path('finder/', include([
+        path('admin/', admin.site.urls),
+        path('api/v1/', include("core.api.v1.urls")),
+        path('api/docs/', include("core.api.docs.urls")),
+    ]))
 ]
